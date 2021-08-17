fb17056173170a51fe0a1c472f44f8d3e97376f1
Derek So <derek.so@suse.com>
2021-08-17 13:02:33 +0800

    update jenkins deploy script

---
+++ setup/jenkins/01-build-jenkins-docker-image.sh
@@ -0,0 +1,15 @@
+#! /bin/bash -e
+
+source ~/myharbor.sh
+
+echo "Build my jenkins image with my own plugins..."
+sudo docker build -t myjenkins:v1.0 -f Dockerfile.x86 .
+
+echo "Login to Harbor private registry ..."
+sudo docker login $HARBOR_URL -u $HARBOR_USR -p $HARBOR_PWD
+
+echo "Upload myjenkins image to Harbor private registry ..."
+sudo docker tag myjenkins:v1.0 $HARBOR_URL/library/myjenkins:v1.0
+sudo docker push $HARBOR_URL/library/myjenkins:v1.0
+
+
--- setup/jenkins/02-install-jenkins.sh
+++ setup/jenkins/02-install-jenkins.sh
@@ -1,14 +1,22 @@
 #! /bin/bash -e
 
+export KUBECONFIG=$HOME/devsecops.cfg
+source $HOME/myharbor.sh
+
+echo Create jenkins namespace
 kubectl create ns jenkins
 
+echo Create service account for jenkins
 kubectl apply -f https://raw.githubusercontent.com/jenkins-infra/jenkins.io/master/content/doc/tutorials/kubernetes/installing-jenkins-on-kubernetes/jenkins-sa.yaml
 
+echo Add jenkins helm repo
 helm repo add jenkinsci https://charts.jenkins.io
 helm repo update
 helm search repo jenkinsci
 
-helm install jenkins jenkinsci/jenkins -n jenkins -f jenkins-values.yaml 
+echo Customize jenkins-values.yaml
+cat jenkins-values-template.yaml > my-jenkins-values.yaml
+helm install jenkins jenkinsci/jenkins -n jenkins -f my-jenkins-values.yaml 
 
 # admin password
 kubectl exec --namespace jenkins -it svc/jenkins -c jenkins -- /bin/cat /run/secrets/chart-admin-password && echo
