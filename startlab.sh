77498cafa914d94abef07b92ab85c2ab968f9189
Derek So <derek.so@suse.com>
2021-08-26 00:21:35 +0800

    move scripts sub-directory into root

--- startlab.sh
+++ startlab.sh
@@ -136,7 +136,7 @@ for vm in rancher harbor; do
   done
   scp $SSH_OPTS -i mylab.key mylab.key ec2-user@$VM_IP:~/.ssh/
   scp $SSH_OPTS -i mylab.key mylab-ssh-config ec2-user@$VM_IP:~/.ssh/config
-  scp $SSH_OPTS -i mylab.key ../setup/$vm/*.*  ec2-user@$VM_IP:~/
+  scp $SSH_OPTS -i mylab.key setup/$vm/*.*  ec2-user@$VM_IP:~/
 done 
 
 # upload files to be deployed onto devsecops cluster
@@ -144,9 +144,9 @@ echo "Upload files to be executed onto devsecops cluster into harbor instance ..
 HARBOR_IP=`get-vm-public-ip $VM_PREFIX-harbor`
 ssh $SSH_OPTS -i mylab.key ec2-user@$HARBOR_IP mkdir -p devsecops/{jenkins,sonarqube,anchore}
 scp $SSH_OPTS -i mylab.key mylab_vm_list.txt ec2-user@$HARBOR_IP:~
-scp $SSH_OPTS -i mylab.key ../setup/jenkins/*.* ec2-user@$HARBOR_IP:~/devsecops/jenkins
-scp $SSH_OPTS -i mylab.key ../setup/sonarqube/*.* ec2-user@$HARBOR_IP:~/devsecops/sonarqube
-scp $SSH_OPTS -i mylab.key ../setup/anchore/*.* ec2-user@$HARBOR_IP:~/devsecops/anchore
+scp $SSH_OPTS -i mylab.key setup/jenkins/*.* ec2-user@$HARBOR_IP:~/devsecops/jenkins
+scp $SSH_OPTS -i mylab.key setup/sonarqube/*.* ec2-user@$HARBOR_IP:~/devsecops/sonarqube
+scp $SSH_OPTS -i mylab.key setup/anchore/*.* ec2-user@$HARBOR_IP:~/devsecops/anchore
 
 
 # write ssh file for easy access
