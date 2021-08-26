4b21350fbf4a4b903c1469343867b0319b0442a5
Derek So <derek.so@suse.com>
2021-08-26 22:34:07 +0800

    configure jenkins-workers namespace

--- setup/jenkins/99-one-step-install-jenkins.sh
+++ setup/jenkins/99-one-step-install-jenkins.sh
@@ -69,3 +69,43 @@ echo Password: $JENKINS_PWD >> ~/myjenkins.txt
 echo
 cat ~/myjenkins.txt
 
+#! /bin/bash -e
+
+export KUBECONFIG=$HOME/.kube/devsecops.cfg
+source $HOME/myharbor.sh
+
+# Create jenkins-workers namespace
+kubectl create ns jenkins-workers
+
+# Create PVC for m2 (java maven2 PV)
+echo "Create PVC m2 (java maven2)..."
+cat <<EOF | kubectl apply -f -
+apiVersion: v1
+kind: PersistentVolumeClaim
+metadata:
+  name: m2
+  namespace: jenkins-workers
+spec:
+  accessModes:
+    - ReadWriteOnce
+  volumeMode: Filesystem
+  resources:
+    requests:
+      storage: 5Gi
+EOF
+
+# Create configmap for docker-config
+echo "Create configmap docker-config ..."
+sudo cat /root/.docker/config.json > $PWD/config.json
+kubectl create configmap docker-config --from-file=config.json -n jenkins-workers
+
+# CA bundle
+echo "Create secret ca-bundle ..."
+openssl s_client -showcerts -connect $HARBOR_URL < /dev/null | sed -ne '/-BEGIN CERTIFICATE-/,/-END CERTIFICATE-/p' > additional-ca-cert-bundle.crt
+kubectl create secret generic ca-bundle --from-file=additional-ca-cert-bundle.crt -n jenkins-workers
+
+# clean up
+rm -f config.json
+rm -f additional-ca-cert-bundle.crt
+
+
